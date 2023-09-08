6030f7d5098fe600be3168235e132da912c81f9b
Guimaraes <claudiosgn02@gmail.com>
2023-09-08 02:37:54 -0300

    Aula 18 - Operadores Pt4 - Lógicos OR.

--- src/academy/devg/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devg/maratonajava/introducao/Aula04Operadores.java
@@ -1,12 +1,12 @@
 package academy.devg.maratonajava.introducao;
 
 public class Aula04Operadores {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         // + - / *
         int numero01 = 10;
         double numero02 = 20;
         double resultado = numero01 / numero02;
-        System.out.println("O resultado é: "+resultado);
+        System.out.println("O resultado é: " + resultado);
 
         // Resto: %
         int resto = 20 % 7;
@@ -29,7 +29,13 @@ public class Aula04Operadores {
         float salario = 3500F;
         boolean isDentroDaLeiMa30 = idade >= 30 && salario >= 4612;
         boolean isDentroDaLeiMe30 = idade < 30 && salario >= 3381;
-        System.out.println("isDentroDaLeiMa30 "+isDentroDaLeiMa30);
-        System.out.println("isDentroDaLeiMe30 "+isDentroDaLeiMe30);
+        System.out.println("isDentroDaLeiMa30 " + isDentroDaLeiMa30);
+        System.out.println("isDentroDaLeiMe30 " + isDentroDaLeiMe30);
+
+        double valorTotalCCorrente = 200;
+        double valorTotalCPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystation5Affordable = valorTotalCCorrente > valorPlaystation || valorTotalCPoupanca > valorPlaystation;
+        System.out.println("isPlaystation5Affordable "+isPlaystation5Affordable);
     }
 }
