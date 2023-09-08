ee5d35239d57222f41a962bf7d8c5e42ccd733dd
Guimaraes <claudiosgn02@gmail.com>
2023-09-08 00:54:42 -0300

    Aula 11 - Tipos Primitivos Pt2.

--- src/academy/devg/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devg/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,16 @@ public class Aula02TiposPrimitivos {
     public static void main(String[]args) {
         // int, double, float, char, byte, short, long, boolean
         int age = 10;
+        long numeroGrande = 10000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
         System.out.println("A idade é "+age);
+        System.out.println(verdadeiro);
+        System.out.println(caractere);
     }
 }
