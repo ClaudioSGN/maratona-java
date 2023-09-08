8d744dba029f88e7b1764a1dbd08244c06e3999b
Guimaraes <claudiosgn02@gmail.com>
2023-09-08 02:10:29 -0300

    Aula 16 - Operadores Pt2 - Relacionais.

--- src/academy/devg/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devg/maratonajava/introducao/Aula04Operadores.java
@@ -7,5 +7,21 @@ public class Aula04Operadores {
         double numero02 = 20;
         double resultado = numero01 / numero02;
         System.out.println("O resultado é: "+resultado);
+
+        // Resto: %
+        int resto = 20 % 7;
+        System.out.println(resto);
+
+        // Lógicos: < > | <= >= == | !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezEqualQueVinte = 10 == 20;
+        boolean isDezEqualQueDez = 10 == 10;
+        boolean isDezDiferenteQueDez = 10 != 10;
+        System.out.println(isDezMaiorQueVinte);
+        System.out.println(isDezMenorQueVinte);
+        System.out.println(isDezEqualQueVinte);
+        System.out.println(isDezEqualQueDez);
+        System.out.println(isDezDiferenteQueDez);
     }
 }
