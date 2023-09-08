d1417b7e56fb216c31fe26ac014ed79385f7e875
Guimaraes <claudiosgn02@gmail.com>
2023-09-08 00:36:19 -0300

    Initial commit

---
+++ src/academy/devg/maratonajava/introducao/Aula01OlaDevG.java
@@ -0,0 +1,15 @@
+package academy.devg.maratonajava.introducao;
+/**
+ * Isto é um comentário <b>javadoc</b>
+ */
+public class Aula01OlaDevG {
+    // Isto é um comentário de uma linha!
+    /*
+        Isso é um comentário
+        de multiplas
+        linhas!
+     */
+    public static void main(String[]args){
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
