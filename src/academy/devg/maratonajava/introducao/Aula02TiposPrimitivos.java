a7a14f2d7b1be6f41a773ed9681d3595fd71ccf0
Guimaraes <claudiosgn02@gmail.com>
2023-09-08 01:15:52 -0300

    Aula 12 - Tipos primitivos Pt3 - Forçando os valores com casting.

--- src/academy/devg/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devg/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,17 +3,20 @@ package academy.devg.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[]args) {
         // int, double, float, char, byte, short, long, boolean
-        int age = 10;
-        long numeroGrande = 10000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        int age = (int) 10000000000L;
+        long numeroGrande = (long )155.23D;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = (float) 2500.0D;
         byte idadeByte = -128;
-        short idadeShort = 10;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = 'M';
         System.out.println("A idade é "+age);
         System.out.println(verdadeiro);
         System.out.println(caractere);
+        System.out.println(salarioFloat);
+        System.out.println(numeroGrande);
+        //O Cast pode ser feito de números maiores para uma variável primitiva menor, porém há chances do resultado ser "quebrado.
     }
 }
